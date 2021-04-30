2568468b121b6bef7021bfbc9b48b592eac67bd6
RomaRoma <v12nes11@gmail.com>
2021-04-30 17:10:10 +0300

    2d practice fin

--- js/script.js
+++ js/script.js
@@ -10,21 +10,34 @@ const personalMovieDB = {
     privat: false
 }
 
-for (let i = 0; i < 2; i++) {
-    let a = prompt("Один из просмотренных фильмов?"),
-        b = prompt("На сколько вы его оцените?");
+// for (let i = 0; i < 2; i++) {
+//     let a = prompt("Один из просмотренных фильмов?"),
+//         b = prompt("На сколько вы его оцените?");
         
-    if (a != '' && a != null && b != '' && b != null) {
-        personalMovieDB.movies[a]=b;
-        console.log('---T---');
-    } else {
-        console.log('---F---');
-        i--;
-    }
-}
+//     if (a != '' && a != null && b != '' && b != null) {
+//         personalMovieDB.movies[a]=b;
+//         console.log('---T---');
+//     } else {
+//         console.log('---F---');
+//         i--;
+//     }
+// }
 
-if (personalMovieDB.count > 10) {
-    console.log('<10');
-} elseif {}
+let x = (personalMovieDB.count < 10) ? console.log('Просмотрено довольно мало фиильмов'):
+        (personalMovieDB.count > 10 && personalMovieDB.count < 30) ? console.log('Вы классический зритель'):
+        (personalMovieDB.count > 30) ? console.log('Вы киноман'):
+        console.log('ошибка');
+
+// (personalMovieDB.count < 10) ? console.log('Просмотрено довольно мало фиильмов') else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) ? console.log('Вы классический зритель') else if (personalMovieDB.count > 30) ? console.log('Вы киноман') : console.log('ошибка')
+
+// if (personalMovieDB.count < 10) {
+//     console.log('Просмотрено довольно мало фиильмов');
+// } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+//     console.log('Вы классический зритель');
+// } else if (personalMovieDB.count > 30) {
+//     console.log('Вы киноман');
+// } else {
+//     console.log('Ошибка алло');
+// }
 
 console.log(personalMovieDB);
